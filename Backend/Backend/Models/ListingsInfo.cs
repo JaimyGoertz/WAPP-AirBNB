851683d1ad0e9c485976c27e0ef14248c84b5633
Jaimy Goertz <jaimygoertz@gmail.com>
2019-12-14 11:38:51 +0100

    added database context

---
+++ Backend/Backend/Models/ListingsInfo.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Models
+{
+    public class ListingsInfo
+    {
+        public ListingsInfo(int id, string listingUrl, string name, double? latitude, double? longitude)
+        {
+            Id = id;
+            ListingUrl = listingUrl;
+            Name = name;
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+
+        [JsonProperty(PropertyName = "Id")]
+        public int Id { get; set; }
+
+        [JsonProperty(PropertyName = "ListingUrl")]
+        public string ListingUrl { get; set; }
+
+        [JsonProperty(PropertyName = "Name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "Latitude")]
+        public double? Latitude { get; set; }
+
+        [JsonProperty(PropertyName = "Longitude")]
+        public double? Longitude { get; set; }
+    }
+}
---
+++ Backend/Backend/Models/SummaryReviews.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Models
+{
+    public partial class SummaryReviews
+    {
+        public int ListingId { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
